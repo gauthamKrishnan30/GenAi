1d1928fe31fd00b6baa32267f57595d3c642ea62
gauthamKrishnan30 <gautham2004k@gmail.com>
2026-03-10 22:17:54 +0530

    API

---
+++ chatbox.py
@@ -0,0 +1,18 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model=genai.GenerativeModel("gemini-2.5-flash")
+
+chat=model.start_chat(history=[])
+
+print("\n****** CHATBOT HAS STARTED  ******\n")
+
+while True:
+    user_input=input("Bot: Ask questions....!:")
+    if user_input.lower()=="exit":
+        break
+    response=chat.send_message(user_input)
+    print("Bot: ",response.text)
