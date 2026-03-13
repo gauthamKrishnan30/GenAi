74a474abc18331f08c071a66873d227812a3ccc9
gauthamKrishnan30 <gautham2004k@gmail.com>
2026-03-13 22:41:50 +0530

    user input

---
+++ UserInput.py
@@ -0,0 +1,13 @@
+def userprompt(user_input):
+    prompt=f"""
+    you are a Ola electric bike sales executive,
+
+    Rules:
+    -Explains the advantages of electric bike
+    -List out the models available
+    -expalin the models features one by one
+    -keep the answer brief and clear to the customers
+
+    question:{user_input}
+    """
+    return prompt
--- chatbox.py
+++ chatbox.py
@@ -1,6 +1,7 @@
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+from UserInput import userprompt
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
@@ -14,5 +15,7 @@ while True:
     user_input=input("Bot: Ask questions....!:")
     if user_input.lower()=="exit":
         break
-    response=chat.send_message(user_input)
+    prompt = userprompt(user_input)
+    response=chat.send_message(prompt)
+
     print("Bot: ",response.text)
